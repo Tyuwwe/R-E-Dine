2ac3ddf16599c5328f186db8c4ca4f5e5ed0a316
Zeki T. Luan <s1324151534@outlook.com>
2023-10-16 12:02:51 +0800

    Update 1.1
    增加了按钮选择的动画

--- index-renderer.js
+++ index-renderer.js
@@ -3,3 +3,31 @@ document.getElementById('closeIndexWindow').addEventListener('click', () => {
     //console.log("func1");
     window.REDAPI.closeindex();
 })
+
+var orderbtn = document.getElementsByClassName('order-list');
+for (i = 0; i < orderbtn.length; i++) {
+    orderbtn[i].addEventListener('click', (btn) => {
+        var select = document.getElementById(btn.target.id);
+        if( select.className == "order-list" ) {
+            select.setAttribute("class", "order-list-clicked");
+        }
+        else {
+            select.setAttribute("class", "order-list");
+        }
+    })
+}
+
+var restbtn = document.getElementsByClassName('rest-list-btn');
+for (i = 0; i < restbtn.length; i++) {
+    restbtn[i].addEventListener('click', (btn) => {
+        var select = document.getElementById(btn.target.id);
+        if( select.className == "rest-list-btn" ) {
+            select.setAttribute("class", "rest-list-btn-selected");
+            select.innerHTML = "Selected";
+        }
+        else {
+            select.setAttribute("class", "rest-list-btn");
+            select.innerHTML = "Select";
+        }
+    })
+}
