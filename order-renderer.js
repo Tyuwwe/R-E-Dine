ece4cbe7236eb6080e17172dd109b195556cd850
Zeki T. Luan <s1324151534@outlook.com>
2023-10-17 17:01:07 +0800

    Update 1.1.4b
    修元素bug
    增加了鼠标位移动画（js实现）
    调整了CSS参数（全局阻止选中）

--- order-renderer.js
+++ order-renderer.js
@@ -1,3 +1,17 @@
 document.getElementById('closeWindow').addEventListener('click', () => {
     window.close();
 })
+
+const picbox = document.getElementById("map-box");
+const pic = document.getElementById("map-img");
+picbox.onmousemove = function(e) {
+    var x = e.clientX / document.body.clientWidth;
+    var y = e.clientY / document.body.clientHeight;
+    //console.log(x);
+    //console.log(y);
+    pic.style.transform = "translate(" + ((x - 0.5) / 8) * document.body.clientWidth + "px, " + ((y - 0.5) / 8) * document.body.clientHeight + "px) scale(1.2)";
+}
+
+picbox.onmouseleave = function(e) {
+    pic.style.transform = "translate(0) scale(1.0)";
+}
